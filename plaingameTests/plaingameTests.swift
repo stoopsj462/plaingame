8f43c70c5fe7f652c313c5659c5b25c44efff780
jason <stoopsj462@gmail.com>
2026-07-28 19:26:32 -0400

    Initial Commit

---
+++ plaingameTests/plaingameTests.swift
@@ -0,0 +1,18 @@
+//
+//  plaingameTests.swift
+//  plaingameTests
+//
+//  Created by jason stoops on 7/28/26.
+//
+
+import Testing
+
+struct plaingameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
